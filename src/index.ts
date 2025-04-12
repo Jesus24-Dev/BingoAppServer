731279f4a9f0388f19f1697e1fa8fce76477f2d5
Jesús Sirit <siritjesus24@gmail.com>
2025-04-11 23:32:02 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: 'https://bingo-app-nine.vercel.app',
   methods: ['GET', 'POST'],
   credentials: true
 }));
